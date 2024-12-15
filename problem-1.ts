97aa96b73cd4eee5f8320d2f7686738f753d411d
bayazidhassan <bayazidhassan776@gmail.com>
2024-12-16 04:39:12 +0600

    first commit

---
+++ problem-1.ts
@@ -0,0 +1,12 @@
+
+const sumArray = (arr: number[]): number => {
+    const size = arr.length;
+    let sum = 0;
+    for (let i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+const result = sumArray([1, 2, 3, 4, 5]);
+console.log(result);
